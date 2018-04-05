6e5f55cd13e4cf39a3d0228c20ce8970bce71d66
fanzs <1326945754@qq.com>
2018-04-05 21:51:57 +0800

    public void main()

---
+++ src/main/java/com/fanzs/secondskill/demo/Main.java
@@ -0,0 +1,10 @@
+package com.fanzs.secondskill.demo;
+
+/**
+ * Created by fzs on 2018/4/5.
+ */
+public class Main {
+    public void main(String[] args) {
+        System.out.println("hello private void main();");
+    }
+}
